da16fd632845fc8ee518f1ce26530ed69c2a90f6
y22jun <tlsdpwns0612@naver.com>
2025-08-20 20:02:08 +0900

    docs: wish-location 스웨거 문서화 작업

---
+++ src/main/java/team/mut4/trip/domain/wishlocation/presentation/WishLocationDocsController.java
@@ -0,0 +1,46 @@
+package team.mut4.trip.domain.wishlocation.presentation;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import team.mut4.trip.domain.food.dto.response.FoodBasicResponse;
+import team.mut4.trip.domain.wishlocation.dto.request.WishLocationSaveRequest;
+import team.mut4.trip.domain.wishlocation.dto.response.WishLocationSaveResponse;
+
+import java.util.List;
+
+@Tag(name = "WishLocation", description = "가고 싶은 장소 관련 API")
+@RequestMapping("/wish-locations")
+public interface WishLocationDocsController {
+
+    @Operation(summary = "가고 싶은 장소 저장", description = "클라이언트가 선택한 가고 싶은 장소를 저장합니다. (클라이언트의 위도 경도 받는다.)")
+    @PostMapping
+    ResponseEntity<WishLocationSaveResponse> saveWishLocation(
+            @RequestBody WishLocationSaveRequest request
+    );
+
+    @Operation(summary = "가고 싶은 장소 기반 주변 음식점 조회 후 저장 (5개만 추출)", description = "선택한 가고 싶은 장소 주변의 음식점을 조회 후 DB에 저장합니다.")
+    @GetMapping("/{wishLocationId}/nearby-food")
+    ResponseEntity<List<FoodBasicResponse>> getNearbyFoodPlaces(
+            @Parameter(description = "조회할 가고 싶은 장소 ID") @PathVariable Long wishLocationId,
+            @Parameter(description = "조회 반경(m)") @RequestParam(defaultValue = "2000") int radius
+    );
+
+    @Operation(summary = "가고 싶은 장소 기반 주변 음식점 조회 후 저장 (모두 추출)", description = "선택한 가고 싶은 장소 주변의 모든 음식점을 조회 후 DB에 저장합니다.")
+    @GetMapping("/{wishLocationId}/nearby-food-all")
+    ResponseEntity<List<FoodBasicResponse>> getNearbyAllFoodPlacesAndSaveFood(
+            @Parameter(description = "조회할 가고 싶은 장소 ID") @PathVariable Long wishLocationId,
+            @Parameter(description = "조회 반경(m)") @RequestParam(defaultValue = "2000") int radius
+    );
+
+    @Operation(summary = "가고 싶은 장소 기반 음식 검색 후 저장", description = "선택한 가고 싶은 장소에서 키워드 기반 음식점 검색 후 DB에 저장합니다.")
+    @GetMapping("/{wishLocationId}/search/food/save")
+    ResponseEntity<List<FoodBasicResponse>> searchAndSaveFood(
+            @Parameter(description = "조회할 가고 싶은 장소 ID") @PathVariable Long wishLocationId,
+            @Parameter(description = "검색 키워드") @RequestParam(defaultValue = "맛집") String keyword,
+            @Parameter(description = "조회 반경(m)") @RequestParam(defaultValue = "2000") int radius
+    );
+
+}
