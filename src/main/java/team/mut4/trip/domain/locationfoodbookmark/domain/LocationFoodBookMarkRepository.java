7e3e563b68928ce8eca53487097d2567e56c9e03
y22jun <tlsdpwns0612@naver.com>
2025-08-22 23:34:34 +0900

    feat: 현재 위치 기반 음식점 북마크 모두 저장 조회 기능 구현

--- src/main/java/team/mut4/trip/domain/locationfoodbookmark/domain/LocationFoodBookMarkRepository.java
+++ src/main/java/team/mut4/trip/domain/locationfoodbookmark/domain/LocationFoodBookMarkRepository.java
@@ -3,10 +3,14 @@ package team.mut4.trip.domain.locationfoodbookmark.domain;
 import team.mut4.trip.domain.food.domain.Food;
 import team.mut4.trip.domain.location.domain.Location;
 
+import java.util.List;
+
 public interface LocationFoodBookMarkRepository {
 
     void save(LocationFoodBookMark locationFoodBookMark);
 
     LocationFoodBookMark findByLocationAndFood(Location location, Food food);
+
+    List<LocationFoodBookMark> findAllByLocation(Location location);
 
 }
